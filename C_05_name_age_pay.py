eb951bf86db67882ed991e2288999c40132eb28e
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2025-03-13 14:06:50 +1300

    Add files via upload

--- C_05_name_age_pay.py
+++ C_05_name_age_pay.py
@@ -28,6 +28,53 @@ def not_blank(question):
         print("Sorry, this can't be blank. Please try again.\n")
 
 
+def string_check(question, valid_answers=('yes', 'no'), num_letters=1):
+
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_answers:
+
+            # check if the response is the entire word
+            if response == item:
+                return item
+
+            # check if it's the 'n' letters
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"Please choose an option from {valid_answers}")
+
+
+# pay_method = string_check("Payment method: ", payment_ans, 2)
+# print(f"You chose {pay_method}")
+
 # main routine starts here
-who = not_blank("Please enter your name: ")
-print(f"Hello {who}")
+
+# initialise variables / non-default options for string checker
+payment_ans = ['cash', 'credit']
+
+# loop for testing purposes
+while True:
+    print()
+
+    # ask user for their name (and check it's not blank)
+    name = not_blank("Name: ")
+
+    # Ask user for their age and check it's between 12 and 120
+    age = int_check("Age: ")
+
+    # Output error message / success message
+    if age < 12:
+        print(f"{name} is to young")
+        continue
+    elif age > 120:
+        print(f"{name} is too old")
+        continue
+    else:
+        pass
+
+    # ask user for payment method (cash / credit / ca / cr)
+    pay_method = string_check("Payment method: ", payment_ans, 2)
+    print(f"{name} has bought a ticket ({pay_method})")
