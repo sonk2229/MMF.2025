0de1032e7abe46063d33a91e51b048fd2b9b43ae
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2025-03-18 10:23:12 +1300

    Add files via upload

---
+++ C_08_ticket_price_panda.py
@@ -0,0 +1,152 @@
+import pandas
+
+
+# Functions go here
+def int_check(question):
+    """Checks users enter an integer"""
+
+    error = "Oops - Please enter an number more than zero."
+
+    while True:
+
+        try:
+            # Return the response if it's an integer
+            response = int(input(question))
+
+            return response
+
+        except ValueError:
+            print(error)
+
+
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be blank. Please try again.\n")
+
+
+def string_check(question, valid_answers=('yes', 'no'), num_letters=1):
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_answers:
+
+            # check if the response is the entire word
+            if response == item:
+                return item
+
+            # check if it's the 'n' letters
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"Please choose an option from {valid_answers}")
+
+
+# pay_method = string_check("Payment method: ", payment_ans, 2)
+# print(f"You chose {pay_method}")
+
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
+
+
+# main routine starts here
+
+# initialise variables / non-default options for string checker
+payment_ans = ['cash', 'credit']
+
+# Ticket Price List
+CHILD_PRICE = 7.50
+ADULT_PRICE = 10.50
+SENIOR_PRICE = 6.50
+
+# Credit card surcharge (currently 5%)
+CREDIT_SURCHARGE = 0.05
+
+# lists to hold tickets details
+all_names = []
+all_ticket_costs = []
+all_surcharges = []
+
+mini_movie_dict = {
+    'Name': all_names,
+    'Ticket Price': all_ticket_costs,
+    'Surcharge': all_surcharges
+}
+
+# loop for testing purposes
+while True:
+    print()
+
+    # ask user for their name (and check it's not blank)
+    name = not_blank("Name: ")
+    if name == "xxx":
+        break
+
+    # Ask user for their age and check it's between 12 and 120
+    age = int_check("Age: ")
+
+    # Output error message / success message
+    if age < 12:
+        print(f"{name} Sorry you are too young for this movie")
+        continue
+
+    # Child ticket price is $7.50
+    elif 12 <= age < 16:
+        ticket_price = CHILD_PRICE
+
+    # Adult ticket ($10.50)
+    elif 16 <= age < 65:
+        ticket_price = ADULT_PRICE
+
+    # Senior Citizen ticket ($6.50)
+    elif 65 <= age < 121:
+        ticket_price = SENIOR_PRICE
+    else:
+        print(f"{name} ?? That looks like typo (too old)")
+        continue
+
+    # ask user for payment method (cash / credit / ca / cr)
+    pay_method = string_check("Payment method: ", payment_ans, 2)
+
+    if pay_method == "cash":
+        surcharge = 0
+
+    # if paying by credit, calculate surcharge
+    else:
+        surcharge = ticket_price * CREDIT_SURCHARGE
+
+    # add name, ticket cost and surcharge to
+    all_names.append(name)
+    all_ticket_costs.append(ticket_price)
+    all_surcharges.append(surcharge)
+
+# create dataframe / table from dictionary
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+
+# Calculate the total payable for each ticket
+mini_movie_frame['Total'] = mini_movie_frame['Ticket Price'] + mini_movie_frame['Surcharge']
+mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
+
+# Work out total paid and total profit....
+total_paid = mini_movie_frame['Total'].sum()
+total_profit = mini_movie_frame['Profit'].sum()
+
+# Currency Formatting (uses currency function)
+add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+# Output movie frame without index
+print(mini_movie_frame.to_string(index=False))
+
+print()
+print(f"Total Paid ${total_paid:.2f}")
+print(f"Total Profit ${total_profit:.2f}")
